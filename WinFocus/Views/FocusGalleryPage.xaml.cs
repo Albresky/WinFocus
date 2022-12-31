a9aa8d05776e8260ffc3fd935d36fd001d29a540
Albresky <78648318+Albresky@users.noreply.github.com>
2022-12-31 19:38:53 +0800

    - sync ListView SelectedIndex with FlipView

--- WinFocus/Views/FocusGalleryPage.xaml.cs
+++ WinFocus/Views/FocusGalleryPage.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Input;
-using WinFocus.Core.Models;
 using WinFocus.ViewModels;
 
 namespace WinFocus.Views;
@@ -9,7 +7,7 @@ namespace WinFocus.Views;
 public sealed partial class FocusGalleryPage : Page
 {
     private WallpaperStyle style = WallpaperStyle.Fill;
-    
+
     public FocusGalleryViewModel ViewModel
     {
         get;
@@ -24,18 +22,18 @@ public sealed partial class FocusGalleryPage : Page
 
     private void DropDownItemClicked(object sender, RoutedEventArgs e)
     {
-        if(Enum.TryParse<WallpaperStyle>((sender as MenuFlyoutItem).Text.ToString(), out style))
+        if (Enum.TryParse<WallpaperStyle>((sender as MenuFlyoutItem).Text.ToString(), out style))
         {
+            var curViewIndex = myFlipView.SelectedIndex;
+            var imagePath = ViewModel.Source.ElementAt(curViewIndex).ImagePath;
+            WallpaperService.Set(imagePath, style);
             Console.WriteLine("selected style:{0}", (sender as MenuFlyoutItem).Text.ToString());
         }
     }
 
-    private void btn_setAsBg_clicked(object sender, RoutedEventArgs e)
+    private void ListsView_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
-        var curViewIndex = myFlipView.SelectedIndex;
-        var imagePath = ViewModel.Source.ElementAt(curViewIndex).ImagePath;
-        WallpaperService.Set(imagePath, WallpaperStyle.Fill);
+        ListView lv = sender as ListView;
+        myFlipView.SelectedIndex = lv.SelectedIndex;
     }
-
-
 }
